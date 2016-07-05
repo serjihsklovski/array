2bc256656debc752edc6b52d534fac017e7029c2
Serjih Sklovski <serjihsklovski@gmail.com>
2016-07-05 10:08:00 +0300

    method Array::push_back was changed

--- array_methods.h
+++ array_methods.h
@@ -1,4 +1,16 @@
 #ifndef ARRAY_METHODS_H
 #define ARRAY_METHODS_H
 
+#include "array.h"
+
+
+template<typename T>
+void Array<T>::push_back(const_reference value) {
+    if (_size == _capacity) {
+        reserve(Array::FACTOR * _capacity);
+    }
+
+    _array[_size++] = value;
+}
+
 #endif // ARRAY_METHODS_H
